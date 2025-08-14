d9489f4c5e5fc911f4e393c0accd362c0d2453d8
d.savuljesku <d.savuljesku@gmail.com>
2025-08-14 16:17:08 +0200

    Connection

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use MediaWiki\MediaWikiServices;
+
 if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIRE_VERSION' ) ) {
 	return;
 }
@@ -8,11 +10,22 @@ define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIRE_VERSION', '1.0.0' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'wire', static function () {
-	$GLOBALS['mwsgWireMessageListeners'] = [];
+	$GLOBALS['mwsgWireServiceUrl'] = '';
+
+	$GLOBALS['wgExtensionFunctions'][] = static function () {
+		$hookContainer = MediaWikiServices::getInstance()->getHookContainer();
+		$hookContainer->register( 'BeforePageDisplay', static function ( OutputPage $out ) {
+			$out->addModules( [ 'mwstake.component.wire' ] );
+			$out->addJsConfigVars( 'mwsgWikiServiceUrl', $GLOBALS['mwsgWireServiceUrl'] );
+		} );
+	};
 
 	$GLOBALS['wgResourceModules']['mwstake.component.wire'] = [
 		'scripts' => [
 			'resources/bootstrap.js'
+		],
+		'dependencies' => [
+			'mwstake.component.tokenAuthenticator'
 		],
 		'localBasePath' => __DIR__
 	];
--- resources/bootstrap.js
+++ resources/bootstrap.js
@@ -1,4 +1,36 @@
-window.mws = window.mws || {};
-window.mws.wire = {
-
+mws = window.mws || {};
+mws.wire = {
+	_isInitialized: false,
+	_subscriptions: {},
+	_url: mw.config.get( 'mwsgWikiServiceUrl' ),
+	_connect: async () => {
+		mws.wire._isInitialized = true;
+		if ( !mws.wire._url ) {
+			return null;
+		}
+		const token = await mws.tokenAuthenticator.generateToken( true );
+		mws.wire.socket = new WebSocket( mws.wire._url + '?token=' + encodeURIComponent( token ) );
+		mws.wire.socket.onopen = () => {
+			console.debug( "Wire connection established." );
+		};
+		mws.wire.socket.onmessage = ( event ) => {
+			console.log( event );
+		};
+		mws.wire.socket.onclose = () => {
+			console.debug( "Wire connection closed." );
+		}
+	},
+	listen: function( channel, callback ) {
+		if ( !mws.wire._isInitialized ) {
+			mws.wire._connect();
+		}
+		if ( !mws.wire._subscriptions[channel] ) {
+			mws.wire._subscriptions[channel] = [];
+		}
+		this._subscriptions[channel].push( callback );
+	}
 };
+
+mws.wire.listen( 'global', function( data ) {
+	console.log( data );
+} );
